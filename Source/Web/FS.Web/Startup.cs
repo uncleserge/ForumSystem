bc8394f2aa649931c69a1e8a3456deb11080a8d9
uncleserge <serge@sadowsky.spb.ru>
2017-09-18 20:25:18 +0300

    WebApp Init

---
+++ Source/Web/FS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FS.Web.Startup))]
+namespace FS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
